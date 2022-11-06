16102f7c92eceb84f0d012b94cddda0962779920
AntonFio <anton_fiolkovski@mail.ru>
2022-11-07 02:02:12 +0300

    feat: add task code 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,46 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true },()=>{})
+
+function del() {
+  fs.readdir(path.join(__dirname, 'files-copy'),
+(error, files) => {
+  if (error) {
+    console.log(error);
+  }
+    files.forEach(file => {
+      if (file) {
+        fs.unlink(path.join(__dirname, 'files-copy',file),
+          (error) => {
+            if (error) {
+              console.log(error);
+            }
+          });
+      }
+    });
+});
+}
+
+function copy() {
+  fs.readdir(path.join(__dirname, 'files'),
+    (error, files) => {
+      if (error)
+        console.log(error);
+      else {
+        files.forEach(file => {
+          if (file) {
+            fs.copyFile(path.join(path.join(__dirname, 'files'), file), path.join(path.join(__dirname, 'files-copy'), file),
+              (error) => {
+                if (error) {
+                  console.log(error);
+                }
+              });
+          }
+        })
+      }
+    });  
+}
+
+del();
+copy();
