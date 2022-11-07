43cc2c8f25d8471742b8f3713f21eeecdcbeb5cb
AntonFio <anton_fiolkovski@mail.ru>
2022-11-07 20:13:29 +0300

    feat: add code task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+
 const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
 
 fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
